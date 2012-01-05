29a14279d16e405583aa603abe7d5d23b7b7b03b
Johs Kristoffersen <johsbk@gmail.com>
2012-01-05 16:29:48 +0100

    fix

--- config.php
+++ config.php
@@ -3,10 +3,8 @@ use main\controllers\Index;
 use templates\TemplatesConfig;
 use main\controllers\Product;
 
-<<<<<<< HEAD
-=======
+
 $local = true;
->>>>>>> ce69f9c94ebb6310d2d566c6e9c6883e9ac50dec
 define("MYSQL_USER","debug");
 define("MYSQL_PASS","debug");
 define("MYSQL_HOST","localhost");
